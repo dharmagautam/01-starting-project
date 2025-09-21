de8e9529d96259a616010bcd8957a724917d6211
Dharma Gautam <gautamdharmaveer22@gmail.com>
2025-09-21 18:35:40 +0530

    used props using external file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,86 @@
-function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+import coreConceptImage from "./assets/react-core-concepts.png"
+import {CORE_CONCEPTS} from './data.js';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+
+    const description = reactDescriptions[genRandomInt(2)];
+    const img = coreConceptImage + '';
+
+    return  <header>
+        <img src={img} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
+            {description} React concepts you will need for almost any app you are
+            going to build!
         </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    </header>;
+}
+
+function CoreConcepts(props) {
+    return (
+        <li>
+            <img src={props.image} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    );
+}
+
+function CoreConcepts1({image, title, description}) {
+    return (
+        <li>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    );
+}
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        {/*<CoreConcepts
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcepts
+                            title={CORE_CONCEPTS[1].title}
+                            description={CORE_CONCEPTS[1].description}
+                            image={CORE_CONCEPTS[1].image}
+                        />
+                        <CoreConcepts
+                            title={CORE_CONCEPTS[2].title}
+                            description={CORE_CONCEPTS[2].description}
+                            image={CORE_CONCEPTS[2].image}
+                        />
+                        <CoreConcepts
+                            title={CORE_CONCEPTS[3].title}
+                            description={CORE_CONCEPTS[3].description}
+                            image={CORE_CONCEPTS[3].image}
+                        />*/}
+
+                        <CoreConcepts {...CORE_CONCEPTS[0]} />
+                        <CoreConcepts {...CORE_CONCEPTS[1]} />
+                        <CoreConcepts {...CORE_CONCEPTS[2]} />
+                        <CoreConcepts1 {...CORE_CONCEPTS[3]} />
+                    </ul>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
